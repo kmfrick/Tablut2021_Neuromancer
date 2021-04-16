9318cbe1a5f0de2b61aaac3530e597aed972608e
kmfrick <kmfrick98@gmail.com>
2021-04-16 12:33:52 +0200

    Add AIMA search submodule

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/client/TablutNeuroWhiteClient.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/client/TablutNeuroWhiteClient.java
@@ -1,11 +1,19 @@
 package it.unibo.ai.didattica.competition.tablut.client;
 
+import aima.core.search.adversarial.*;
+
 import java.io.IOException;
 import java.net.UnknownHostException;
+import it.unibo.ai.didattica.competition.tablut.domain.*;
 
 public class TablutNeuroWhiteClient {
 
+	private final double UTIL_MAX = 1000;
+	private final int TIME_SEC = 10;
+
 	public static void main(String[] args) throws UnknownHostException, ClassNotFoundException, IOException {
+		
+		var search = createFor(GameAshtonTablut, 0, UTIL_MAX, TIME_SEC);
 		System.out.println("NeuroWhiteClient::main() called");
 		String[] array = new String[]{"WHITE"};
 		TablutNeuroClient.main(array);
