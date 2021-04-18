041213945441fc105f79d45489072f6ccd56c385
kmfrick <kmfrick98@gmail.com>
2021-04-18 16:04:41 +0200

    Add MinimaxSearch to TablutNeuroClient
    Delete TablutActionsFunction

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/client/TablutNeuroClient.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/client/TablutNeuroClient.java
@@ -5,10 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.UnknownHostException;
 
-import it.unibo.ai.didattica.competition.tablut.domain.Action;
-import it.unibo.ai.didattica.competition.tablut.domain.State.Turn;
-import it.unibo.ai.didattica.competition.tablut.domain.StateTablut;
-import it.unibo.ai.didattica.competition.tablut.solve.TablutActionsFunction;
+import it.unibo.ai.didattica.competition.tablut.domain.*;
+import it.unibo.ai.didattica.competition.tablut.solve.TablutMinimax;
 
 /**
  * 
@@ -17,24 +15,28 @@ import it.unibo.ai.didattica.competition.tablut.solve.TablutActionsFunction;
  */
 public class TablutNeuroClient extends TablutClient {
 
-	private TablutActionsFunction actionsFunction;
 
-	public TablutNeuroClient(String player) throws UnknownHostException, IOException {
+	private final double UTIL_MAX = 1000;
+	private final int TIME_SEC = 10;
+
+	private TablutMinimax search;
+	private int game;
+
+	public TablutNeuroClient(String player, int game) throws UnknownHostException, IOException {
 		super(player, "Neuromancer");
-		actionsFunction = new TablutActionsFunction();
-
+		this.game = game;
 	}
 
 	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
 
+		int gametype = 4;
 		if (args.length == 0) {
 			System.out.println("You must specify which player you are (WHITE or BLACK)!");
 			System.exit(-1);
 		}
-		System.out.println("Selected this: " + args[0]);
+		System.out.println("Selected client: " + args[0]);
 
-		TablutClient client = new TablutNeuroClient(args[0]);
-
+		TablutClient client = new TablutNeuroClient(args[0], gametype);
 		client.run();
 
 	}
@@ -45,34 +47,53 @@ public class TablutNeuroClient extends TablutClient {
 		System.out.println("You are player " + this.getPlayer().toString() + "!");
 		String actionStringFrom = "";
 		String actionStringTo = "";
-		Action action;
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			this.declareName();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		Action action = null;
+		Game rules = null;
+		State state = null;
+		switch (this.game) {
+			case 1:
+				state = new StateTablut();
+				rules = new GameTablut();
+				break;
+			case 2:
+				state = new StateTablut();
+				rules = new GameModernTablut();
+				break;
+			case 3:
+				state = new StateBrandub();
+				rules = new GameTablut();
+				break;
+			case 4:
+				state = new StateTablut();
+				state.setTurn(State.Turn.WHITE);
+				rules = new GameAshtonTablut(99, 0, "logs", "fake", "fake");
+				System.out.println("Ashton Tablut game");
+				break;
+			default:
+				System.out.println("Error in game selection");
+				System.exit(4);
+		}
 
-		if (this.getPlayer() == Turn.WHITE) {
+
+
+		if (this.getPlayer() == State.Turn.WHITE) {
 			System.out.println("You are player " + this.getPlayer().toString() + "!");
 			while (true) {
 				try {
 					this.read();
 
-					System.out.println("Current state:");
-					System.out.println(this.getCurrentState().toString());
 					if (this.getCurrentState().getTurn().equals(StateTablut.Turn.WHITE)) {
-						System.out.println("Player " + this.getPlayer().toString() + ", do your move: ");
-						for (var act : actionsFunction.getPossibleMoves(this.getCurrentState())) {
-							System.out.println(act);
-						}
-						
-						System.out.println("From: ");
-
-						actionStringFrom = in.readLine();
-						System.out.println("To: ");
-						actionStringTo = in.readLine();
-						action = new Action(actionStringFrom, actionStringTo, this.getPlayer());
+						long startTime = System.currentTimeMillis();
+						Action a = search.makeDecision(state);
+						long estimatedTime = System.currentTimeMillis() - startTime;
+						System.out.println("Played move: " + a.toString());
+						System.out.println("Time to find move: " + estimatedTime/1000.0 + " seconds");
 						this.write(action);
 					} else if (this.getCurrentState().getTurn().equals(StateTablut.Turn.BLACK)) {
 						System.out.println("Waiting for your opponent move... ");
@@ -97,18 +118,12 @@ public class TablutNeuroClient extends TablutClient {
 			while (true) {
 				try {
 					this.read();
-					System.out.println("Current state:");
-					System.out.println(this.getCurrentState().toString());
 					if (this.getCurrentState().getTurn().equals(StateTablut.Turn.BLACK)) {
-						System.out.println("Player " + this.getPlayer().toString() + ", do your move: ");
-						for (var act : actionsFunction.getPossibleMoves(this.getCurrentState())) {
-							System.out.println(act);
-						}
-						System.out.println("From: ");
-						actionStringFrom = in.readLine();
-						System.out.println("To: ");
-						actionStringTo = in.readLine();
-						action = new Action(actionStringFrom, actionStringTo, this.getPlayer());
+						long startTime = System.currentTimeMillis();
+						Action a = search.makeDecision(state);
+						long estimatedTime = System.currentTimeMillis() - startTime;
+						System.out.println("Played move: " + a.toString());
+						System.out.println("Time to find move: " + estimatedTime/1000.0 + " seconds");
 						this.write(action);
 					} else if (this.getCurrentState().getTurn().equals(StateTablut.Turn.WHITE)) {
 						System.out.println("Waiting for your opponent move... ");
