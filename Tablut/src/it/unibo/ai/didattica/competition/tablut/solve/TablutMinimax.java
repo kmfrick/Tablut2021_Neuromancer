210283c7fddf574bf5fddc87cb78bdab9dc5dd0c
kmfrick <kmfrick98@gmail.com>
2021-04-23 17:25:47 +0200

    Use Alpha-Beta search instead of Minimax

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/solve/TablutMinimax.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/solve/TablutMinimax.java
@@ -4,25 +4,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import aima.core.search.adversarial.Game;
-import aima.core.search.adversarial.MinimaxSearch;
+import aima.core.search.adversarial.IterativeDeepeningAlphaBetaSearch;
 
 import it.unibo.ai.didattica.competition.tablut.domain.*;
 
-public class TablutMinimax extends MinimaxSearch<State, Action, String> {
+public class TablutMinimax extends IterativeDeepeningAlphaBetaSearch<State, Action, State.Turn> {
 
-	Game g;
 
-	public TablutMinimax(Game<State, Action, String> game) {
-		super(game);
-		g = game;
+	public TablutMinimax(Game<State, Action, State.Turn> game) {
+		super(game, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 5);
 	}
 
 	@Override
 	public Action makeDecision(State state) {
 		System.out.println("[Neuromancer] makeDecision() called");
-	//	for (var a : g.getActions(state)) {
-	//		System.out.println("[NeuroMinimax]" + a.toString());
-	//	}
 		return super.makeDecision(state);
 	}
 }
